97c21f86459de63c6b156b350e23b16300ac2b1d
Alif_ita <alifipa5@gmail.com>
2023-06-15 21:44:48 +0700

    setup

---
+++ gateway/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const cors = require('cors')
+const proxy = require('express-http-proxy')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.use('/customer', proxy('http://localhost:8001/'))
+app.use('/product', proxy('http://localhost:8002/'))
+app.use('/shopping', proxy('http://localhost:8003/'))
+
+
+app.listen(8000, ()=> {
+    console.log("Gateway is listening on port 8000");
+})
